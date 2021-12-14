7e234eac7733662af66ba7032fd69f61b483f3e3
张博 <5135059+zhangbo2008@user.noreply.gitee.com>
2021-12-14 20:29:07 +0800

    no_comment

---
+++ 1.py
@@ -0,0 +1,26 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import requests
+from PIL import Image
+import torch
+#=========经典的gpt结构.
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
+
+# load image from the IAM dataset
+url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
+image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+
+# training
+model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+model.config.vocab_size = model.config.decoder.vocab_size
+
+pixel_values = processor(image, return_tensors="pt").pixel_values
+text = "hello world"
+labels = processor.tokenizer(text, return_tensors="pt").input_ids
+outputs = model(pixel_values=pixel_values, labels=labels)
+loss = outputs.loss
+
+# inference (generation)
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
